f064caa52bf575c8b83aae75239cd1b42634b32f
o_0 <90154302+EugeneSvetov@users.noreply.github.com>
2022-10-24 21:25:15 +0300

    updated

--- TLS/settings.py
+++ TLS/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%*kau!z*mcvf7=@75vu0gk%!k8oa7_c%w2b7(sd=%tw32k9=hc'
+SECRET_KEY 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
